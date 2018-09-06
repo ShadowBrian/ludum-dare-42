a96cc147f8a41625102fdac3ef106e5016771a09
Brian Lindenhof <brian@shadowbrain.net>
2018-09-06 23:15:02 +0200

    SteamVR conversion of the Player prefab and controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,9 +16,14 @@ public class PlayerController : MonoBehaviour
     public float HookPullMinDistance;
 
     // Self-references
-    public GameObject CameraRoot;
+    public SteamVR_TrackedObject[] Hands = new SteamVR_TrackedObject[2];
+    public SteamVR_Controller.Device[] controllerinputs = new SteamVR_Controller.Device[2];
+
     public LineRenderer HookLineRenderer;
     public Transform HookLineSource;
+
+    public Transform Collider,Head;
+
 
     private Rigidbody _rb;
     private float _rotationY;
@@ -27,7 +33,8 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Lol screw cursor locking in VR
+        //Cursor.lockState = CursorLockMode.Locked;
 
         _hook = Instantiate(HookPrefab);
         _hook.SetActive(false);
@@ -49,11 +56,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    bool GotHands;
+
+    IEnumerator Start() {
+
+        //Wait for both hands to be turned on.
+        while(Hands[0].index == SteamVR_TrackedObject.EIndex.None) {
+            yield return null;
+        }
+
+        while (Hands[1].index == SteamVR_TrackedObject.EIndex.None) {
+            yield return null;
+        }
+        controllerinputs = new SteamVR_Controller.Device[2];
+
+        //Get their input references.
+        controllerinputs[0] = SteamVR_Controller.Input((int)Hands[0].index);
+        controllerinputs[1] = SteamVR_Controller.Input((int)Hands[1].index);
+        GotHands = true;
+    }
+
     void Update()
     {
-        // Mouse look
+        // Mouse look- Completely not needed for VR :)
 
-        if (GameManager.Instance.EnableMouseLook)
+        /*if (GameManager.Instance.EnableMouseLook)
         {
             float rotationX = CameraRoot.transform.localEulerAngles.y + Input.GetAxisRaw("Mouse X") * GameManager.Instance.SensitivityX;
 
@@ -64,7 +91,14 @@ public class PlayerController : MonoBehaviour
             _rotationY = Mathf.Clamp(_rotationY <= 180 ? _rotationY : -360 + _rotationY, -89f, 89f);
 
             CameraRoot.transform.localEulerAngles = new Vector3(-_rotationY, rotationX, 0f);
+        }*/
+
+        //Don't do anything if you have no hands on.
+        if (!GotHands) {
+            return;
         }
+
+        Collider.localPosition = new Vector3(Head.localPosition.x, 0, Head.localPosition.z);
 
         if (GameManager.Instance.InMenu)
         {
@@ -74,16 +108,26 @@ public class PlayerController : MonoBehaviour
         if (!GameManager.Instance.Alive || GameManager.Instance.ExitingLevel || GameManager.Instance.InMenu) return;
 
         // Hook input
-        if (Input.GetMouseButtonDown(0))
+        //Trigger as fire
+        if (controllerinputs[0].GetHairTriggerDown())
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, CameraRoot.transform.forward, out hit, 100f, HookRaycastLayerMask, QueryTriggerInteraction.Ignore))
+            //Raycast from hand
+            if (Physics.Raycast(transform.position, Hands[0].transform.forward - Hands[0].transform.up, out hit, 100f, HookRaycastLayerMask, QueryTriggerInteraction.Ignore))
             {
                 if (hit.collider.gameObject.CompareTag("HookableSurface"))
                 {
+
+                    if (_hookActive) {
+                        _hookActive = false;
+                        _hook.SetActive(false);
+                        HookLineRenderer.enabled = false;
+                    }
+
                     _hook.transform.position = hit.point;
                     _hook.SetActive(true);
                     _hookActive = true;
+                    HookLineSource = Hands[0].transform;
 
                     var sfx = GameManager.Instance.HookSFXPool.GetPooledObject();
                     sfx.SetActive(true);
@@ -91,10 +135,44 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(1))
+        //Grip as let go
+        if (controllerinputs[0].GetPressDown(EVRButtonId.k_EButton_Grip))
         {
             if (_hookActive)
             {
+                _hookActive = false;
+                _hook.SetActive(false);
+                HookLineRenderer.enabled = false;
+            }
+        }
+
+        // Hook input2
+        //Trigger as fire
+        if (controllerinputs[1].GetHairTriggerDown()) {
+            RaycastHit hit;
+            //Raycast from hand
+            if (Physics.Raycast(transform.position, Hands[1].transform.forward - Hands[1].transform.up, out hit, 100f, HookRaycastLayerMask, QueryTriggerInteraction.Ignore)) {
+                if (hit.collider.gameObject.CompareTag("HookableSurface")) {
+
+                    if (_hookActive) {
+                        _hookActive = false;
+                        _hook.SetActive(false);
+                        HookLineRenderer.enabled = false;
+                    }
+
+                    _hook.transform.position = hit.point;
+                    _hook.SetActive(true);
+                    _hookActive = true;
+                    HookLineSource = Hands[1].transform;
+                    var sfx = GameManager.Instance.HookSFXPool.GetPooledObject();
+                    sfx.SetActive(true);
+                }
+            }
+        }
+
+        //Grip as let go
+        if (controllerinputs[1].GetPressDown(EVRButtonId.k_EButton_Grip)) {
+            if (_hookActive) {
                 _hookActive = false;
                 _hook.SetActive(false);
                 HookLineRenderer.enabled = false;
@@ -132,10 +210,10 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMenu()
     {
-        if (Input.GetMouseButtonDown(0) && !GameManager.Instance.MenuPlayPressed)
+        if (controllerinputs[0].GetHairTriggerDown() && !GameManager.Instance.MenuPlayPressed)
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, CameraRoot.transform.forward, out hit, 100f, MenuLayerMask))
+            if (Physics.Raycast(transform.position, Hands[0].transform.forward - Hands[0].transform.up, out hit, 100f, MenuLayerMask))
             {
                 var option = hit.collider.gameObject.tag;
                 if (option == "SensitivityDown")
@@ -167,6 +245,45 @@ public class PlayerController : MonoBehaviour
                 }
                 else if (option == "Exit")
                 {
+                    Application.Quit();
+                }
+
+                GameManager.Instance.SensitivityX = Mathf.Clamp(GameManager.Instance.SensitivityX, 0.1f, 5f);
+                GameManager.Instance.SensitivityY = Mathf.Clamp(GameManager.Instance.SensitivityY, 0.1f, 5f);
+
+                PlayerPrefs.SetFloat("SensitivityX", GameManager.Instance.SensitivityX);
+                PlayerPrefs.SetFloat("SensitivityY", GameManager.Instance.SensitivityY);
+            }
+        }
+        if (controllerinputs[1].GetHairTriggerDown() && !GameManager.Instance.MenuPlayPressed) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Hands[1].transform.forward - Hands[1].transform.up, out hit, 100f, MenuLayerMask)) {
+                var option = hit.collider.gameObject.tag;
+                if (option == "SensitivityDown") {
+                    GameManager.Instance.SensitivityX -= 0.1f;
+                    GameManager.Instance.SensitivityY -= 0.1f;
+
+                    var sfx = GameManager.Instance.MenuSensSFXPool.GetPooledObject();
+                    sfx.transform.position = transform.position;
+                    sfx.SetActive(true);
+                }
+                else if (option == "SensitivityUp") {
+                    GameManager.Instance.SensitivityX += 0.1f;
+                    GameManager.Instance.SensitivityY += 0.1f;
+
+                    var sfx = GameManager.Instance.MenuSensSFXPool.GetPooledObject();
+                    sfx.transform.position = transform.position;
+                    sfx.SetActive(true);
+                }
+                else if (option == "Play") {
+                    GameManager.Instance.GameEnded = false;
+                    GameManager.Instance.MenuPlayPressed = true;
+
+                    var sfx = GameManager.Instance.MenuPlaySFXPool.GetPooledObject();
+                    sfx.transform.position = transform.position;
+                    sfx.SetActive(true);
+                }
+                else if (option == "Exit") {
                     Application.Quit();
                 }
 
